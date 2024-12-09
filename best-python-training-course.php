c11510ff17c382eac29403efbfb46fda1f50e32f
Nikhil <nikhil.vhits@gmail.com>
2024-12-09 16:59:18 +0530

    analytics changes

--- best-python-training-course.php
+++ best-python-training-course.php
@@ -150,9 +150,29 @@
     text-align:justify;
 }
       </style>
+      <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5FWLQ3C');</script>
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-ZDR3DPRCX5"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-ZDR3DPRCX5');
+</script>
+<!-- End Google Tag Manager -->
    </head>
    <body>
-
+ <!-- Google Tag Manager (noscript) -->
+ <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5FWLQ3C"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
       <!--==============================
          Header Area
          ==============================-->
