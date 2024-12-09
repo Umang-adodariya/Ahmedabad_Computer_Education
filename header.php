aa03a3dfc9152fb00a4219ce3fc2f61a19819c0a
Nikhil <nikhil.vhits@gmail.com>
2024-12-09 16:53:22 +0530

    maulik changes || analitics

--- header.php
+++ header.php
@@ -48,6 +48,11 @@
 </head>
 
 <body>
+    <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5FWLQ3C"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
    <!--==============================
      Preloader
   ==============================-->
