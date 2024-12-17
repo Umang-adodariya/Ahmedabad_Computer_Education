4eaa9f7646b55909fbdc4d2a616f669a84ae984f
Nikhil <nikhil.vhits@gmail.com>
2024-12-17 17:28:11 +0530

    set mail issue fixed

--- mail.php
+++ mail.php
@@ -1,158 +1,189 @@
 <?php
 session_start();
 $_SESSION['flash_message'] = 'Our team will review your inquiry and get back to you shortly.';
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
+
 $requestUri = basename($_SERVER['HTTP_REFERER']);
+
 if($requestUri == 'Ahmedabad_Computer_Education'){
     $requestUri = 'index.php';
 }
 // Only process POST reqeusts.
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $location ='';
-        // Get the form fields and remove whitespace.
-        $name = strip_tags(trim($_POST["name"]));
-        $name = str_replace(array("\r","\n"),array(" "," "),$name);
-        $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
-        $subject = trim($_POST["subject"]);
-        $number = trim($_POST["number"]);
-        $message = trim($_POST["message"]);
-        $form = trim($_POST["form"]);
-        $date = trim($_POST["date"]);
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $location ='';
+    // Get the form fields and remove whitespace.
+    $name = strip_tags(trim($_POST["name"]));
+    $name = str_replace(array("\r","\n"),array(" "," "),$name);
+    $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+    $subject = trim($_POST["subject"]);
+    $number = trim($_POST["number"]);
+    $message = trim($_POST["message"]);
+    $form = trim($_POST["form"]);
+    $date = trim($_POST["date"]);
 
-           // $toMail = "career@yopmail.com";
-        // $ccMail = "umang@yopmail.com";
-        $toMail = "career@ahmedabadcomputereducation.com";
-        $ccMail = "info@virtualheight.com";
+    //     $toMail = "career@yopmail.com";
+    // $ccMail = "umang@yopmail.com";
+    $toMail = "career@ahmedabadcomputereducation.com";
+    $ccMail = "info@virtualheight.com";
 
-        if($form == "contact_form"){
-            $subject = "New contact from $subject";
+    if($form == "contact_form"){
+        $subject = "New contact from $subject";
 
-            $email_content = "Name: $name<br>";
-            $email_content .= "Subject: $subject<br>";
-            $email_content .= "Email: $email<br>";
-            $email_content .= "Message:$message";
-            $location = "contact.php";
-        }
-        elseif($form == "get_in_touch"){
-            $subject = "ACE - Inquiry-GetinTouchForm";
+        $email_content = "Name: $name<br>";
+        $email_content .= "Subject: $subject<br>";
+        $email_content .= "Email: $email<br>";
+        $email_content .= "Message:$message";
+        $location = "contact.php";
+    }
+    elseif($form == "get_in_touch"){
+        $subject = "ACE - Inquiry-GetinTouchForm";
 
-            $email_content = "Name: $name<br>";
-            $email_content .= "Number: $number<br>";
-            $location = "index.php";
-
-        }
-        elseif($form == "registration_form"){
-            $subject = "New Registration";
-
-            $email_content = "Name: $name<br>";
-            $email_content .= "Email: $email<br>";
-            $email_content .= "Number: $number<br>";
-            $location = "index.php";
-        }
-        elseif($form == "booking_appointment"){
-            $message = $subject;
-            $subject = "Book New Appointment for $subject";
-
-            $email_content = "Name: $name<br>";
-            $email_content .= "Email: $email<br>";
-            $email_content .= "Date: $date<br>";
-            $email_content .= "Subject: $message<br>";
-            $location = "index.php";
-        }
-        elseif($form == "newsletter_form"){
-            $subject = "News letter subscribe";
-
-            $email_content = "Email: $email";
-            $location = "index.php";
-        }
-        elseif($form == "course_form"){
-            $subject = "New Registration";
-
-            $email_content = "Name: $name<br>";
-            $email_content .= "Email: $email<br>";
-            $email_content .= "Number: $number";
-            $location = $requestUri;
-        }
-        elseif($form == "quick_contact_form"){
-            $subject = "Quick Contact";
-
-            $email_content = "Name: $name<br>";
-            $email_content .= "Email: $email<br>";
-            $email_content .= "Number: $number<br>";
-            $email_content .= "Message: $message";
-            $location = "team-details.php";
-        }
-        elseif($form == "blog_form"){
-            $subject = "Blog Reply";
-
-            $email_content = "Name: $name<br>";
-            $email_content .= "Email: $email<br>";
-            $email_content .= "Message: $message";
-            $location = "blog.php";
-        }
-        
-        require 'vendor/autoload.php';
-        // send grid smtp
-        // $email = new \SendGrid\Mail\Mail();
-        // $email->setFrom("demotest@appworkdemo.com", "Ahmedabad Computer Education");
-        // $email->setSubject($subject);
-        // $email->addTo($recipient, "Recipient Name");
-        // $email->addContent(
-        //     "text/html",$email_content
-        // );
-
-        // $sendgrid = new \SendGrid('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');  // Replace with your SendGrid API key
-
-        // try {
-        //     $response = $sendgrid->send($email);
-        //     // echo '<script>alert(Thank You! Your message has been sent.);</script>';
-        //     header("Location: $location");
-        // } catch (Exception $e) {
-        //     // echo "<script>alert(Oops! Something went wrong and we couldn't send your message.);</script>";
-        //     header("Location: $location");
-        //     // echo 'Caught exception: ' . $e->getMessage() . "\n";
-        // }
-
-        //  aws smtp
-        $mail = new PHPMailer(true);
-
-        try {
-            // Server settings
-            $mail->isSMTP();
-            $mail->Host = 'email-smtp.ap-south-1.amazonaws.com'; // Your SMTP host
-            $mail->SMTPAuth = true;
-            $mail->Username = 'AKIAUEUHDK3KN23UONML'; // Your SMTP username
-            $mail->Password = 'BEZ2V5iFetDaM1DiZJngpRJC2icvEQULZfhZ/6OBVZXu'; // Your SMTP password
-            $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-            $mail->Port = 587;
-        
-            // Sender and recipient settings
-            $mail->setFrom('tilak@web30india.com', 'ACE - VH');
-            $mail->addAddress($toMail, 'career@ahmedabadcomputereducation.com'); // Add recipient
-            $mail->addCC($ccMail, 'info@virtualheight.com'); // Add recipient
-        
-            // Email content
-            $mail->isHTML(true);
-            $mail->Subject = $subject;
-            $mail->Body = $email_content;
-        
-            // Send email
-            $mail->send();
-            $location = $requestUri;
-            header("Location: $location");
-        } catch (Exception $e) {
-            $location = $requestUri;
-            header("Location: $location");
-        }
-
-    } else {
-        // Not a POST request, set a 403 (forbidden) response code.
-        http_response_code(403);
-        $location = $requestUri;
-        // echo "There was a problem with your submission, please try again.";
-        header("Location: $location");
+        $email_content = "Name: $name<br>";
+        $email_content .= "Number: $number<br>";
+        $location = "index.php";
 
     }
+    elseif($form == "registration_form"){
+        $subject = "New Registration";
 
+        $email_content = "Name: $name<br>";
+        $email_content .= "Email: $email<br>";
+        $email_content .= "Number: $number<br>";
+        $location = "index.php";
+    }
+    elseif($form == "booking_appointment"){
+        $message = $subject;
+        $subject = "Book New Appointment for $subject";
+
+        $email_content = "Name: $name<br>";
+        $email_content .= "Email: $email<br>";
+        $email_content .= "Date: $date<br>";
+        $email_content .= "Subject: $message<br>";
+        $location = "index.php";
+    }
+    elseif($form == "newsletter_form"){
+        $subject = "News letter subscribe";
+
+        $email_content = "Email: $email";
+        $location = "index.php";
+    }
+    elseif($form == "course_form"){
+        $subject = "New Registration";
+
+        $email_content = "Name: $name<br>";
+        $email_content .= "Email: $email<br>";
+        $email_content .= "Number: $number";
+        $location = $requestUri;
+    }
+    elseif($form == "quick_contact_form"){
+        $subject = "Quick Contact";
+
+        $email_content = "Name: $name<br>";
+        $email_content .= "Email: $email<br>";
+        $email_content .= "Number: $number<br>";
+        $email_content .= "Message: $message";
+        $location = "team-details.php";
+    }
+    elseif($form == "blog_form"){
+        $subject = "Blog Reply";
+
+        $email_content = "Name: $name<br>";
+        $email_content .= "Email: $email<br>";
+        $email_content .= "Message: $message";
+        $location = "blog.php";
+    }
+    
+    require 'vendor/autoload.php';
+    // send grid smtp
+    // $email = new \SendGrid\Mail\Mail();
+    // $email->setFrom("demotest@appworkdemo.com", "Ahmedabad Computer Education");
+    // $email->setSubject($subject);
+    // $email->addTo($recipient, "Recipient Name");
+    // $email->addContent(
+    //     "text/html",$email_content
+    // );
+
+    // $sendgrid = new \SendGrid('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');  // Replace with your SendGrid API key
+
+    // try {
+    //     $response = $sendgrid->send($email);
+    //     // echo '<script>alert(Thank You! Your message has been sent.);</script>';
+    //     header("Location: $location");
+    // } catch (Exception $e) {
+    //     // echo "<script>alert(Oops! Something went wrong and we couldn't send your message.);</script>";
+    //     header("Location: $location");
+    //     // echo 'Caught exception: ' . $e->getMessage() . "\n";
+    // }
+
+    //  aws smtp
+    // $mail = new PHPMailer(true);
+
+    // try {
+    //     // Server settings
+    //     $mail->isSMTP();
+    //     $mail->Host = 'email-smtp.ap-south-1.amazonaws.com'; // Your SMTP host
+    //     $mail->SMTPAuth = true;
+    //     $mail->Username = 'AKIAUEUHDK3KN23UONML'; // Your SMTP username
+    //     $mail->Password = 'BEZ2V5iFetDaM1DiZJngpRJC2icvEQULZfhZ/6OBVZXu'; // Your SMTP password
+    //     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    //     $mail->Port = 587;
+    
+    //     // Sender and recipient settings
+    //     $mail->setFrom('tilak@web30india.com', 'ACE - VH');
+    //     $mail->addAddress($toMail, 'career@ahmedabadcomputereducation.com'); // Add recipient
+    //     $mail->addCC($ccMail, 'info@virtualheight.com'); // Add recipient
+    
+    //     // Email content
+    //     $mail->isHTML(true);
+    //     $mail->Subject = $subject;
+    //     $mail->Body = $email_content;
+    
+    //     // Send email
+    //     $mail->send();
+    //     $location = $requestUri;
+    //     header("Location: $location");
+    // } catch (Exception $e) {
+    //     $location = $requestUri;
+    //     header("Location: $location");
+    // }
+
+
+    //swift mailer
+    try {
+        $transport = (new Swift_SmtpTransport('smtp.sendgrid.net', 465, 'ssl'))
+            ->setUsername('apikey')
+            ->setPassword('dummy_password');
+        // Create the Mailer using your created Transport
+        $mailer = new Swift_Mailer($transport);
+    
+        // Create a message
+        $message = new Swift_Message();
+    
+        // Setting email content
+        $message->setSubject($subject);
+        $message->setBody($email_content);
+        $message->addPart($email_content, 'text/html');
+    
+        // Setting from and recipient addresses
+        $message->setfrom(['demotest@appworkdemo.com' => 'ACE - VH']);
+        $message->addto($ccMail, 'Virtual Height');
+        $message->addto($toMail, 'Career');
+        //$message->addto("jaybanker.vhits@gmail.com", 'Jay');
+        
+    
+        // Sending the message
+        $result = $mailer->send($message);
+        $location = $requestUri;
+        header("Location: $location");
+    } catch (Exception $e) {
+        echo $e->getMessage();
+        die;
+    }
+
+} else {
+    // Not a POST request, set a 403 (forbidden) response code.
+    http_response_code(403);
+    $location = $requestUri;
+    // echo "There was a problem with your submission, please try again.";
+    header("Location: $location");
+}
 ?>
